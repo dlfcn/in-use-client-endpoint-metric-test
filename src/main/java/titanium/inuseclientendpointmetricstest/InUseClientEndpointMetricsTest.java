6eb66f526ad55c3073c1cf9c7e495be27b6afd3a
dfontana <dfontana@titaniumplatform.com>
2024-12-19 12:43:43 -0500

    Added pom and main.

---
+++ src/main/java/titanium/inuseclientendpointmetricstest/InUseClientEndpointMetricsTest.java
@@ -0,0 +1,8 @@
+package titanium.inuseclientendpointmetricstest;
+
+public class InUseClientEndpointMetricsTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
